9c8b2fbb61bd2d7f772e1339b21e61b85ec444de
Helen Root <helenroot1@gmail.com>
2022-02-28 18:53:48 +0100

    Add area getter

--- data/index.js
+++ data/index.js
@@ -1,12 +1,58 @@
 const csv = require('csvtojson')
 
-async function getGroundCarbonDensity(location, groundType) {
+// Gets the carbon area density of a given ground type.
+async function getCarbonDensity(location, groundType) {
   const csvFilePath = './data/dataByEpci/ground.csv'
   const groundDataByEpci = await csv().fromFile(csvFilePath)
-  const groundData = groundDataByEpci.find(data => data.epciSiren === location.epci)
+  const groundData = groundDataByEpci.find(data => data.siren === location.epci)
   return parseFloat(groundData[groundType])
 }
 
+// Gets the area in hectares (ha) of a given ground type in a location.
+// The ground types Corine Land Cover uses are different from the types used by Aldo,
+// so a mapping is used and the sum of ha of all matching CLC types is returned.
+// NB: in the lookup the type names for ground data and the more specific biomass data
+// are placed on the same level, so some CLC codes are used in two types.
+async function getArea(location, groundType) {
+  // consider making this a separate json file for isolation
+  // TODO: make more standardised keys?
+  const aldoTypeToClcCodes = {
+    "cultures": ["211", "212", "213", "241", "242", "243", "244"],
+    "prairies": ["231", "321", "322", "323"],
+    "prairies herbacées": ["231", "321"],
+    "prairies arbustive": ["322"],
+    "prairies arborée": ["323"],
+    "forêts": ["311", "312", "313", "324"],
+    "forêts feuillus": ["311", "324"],
+    "forêts conifères": ["312"],
+    "forêts mixte": ["313"],
+    "vignes": ["221"],
+    "vergers": ["222", "223"],
+    "sols artificiels imperméabilisés": ["111", "121", "122", "123", "124", "131", "132", "133", "142"],
+    "sols artificialisés": ["112"],
+    "sols arborés": ["141"], // aka "sols artificiels arborés et buissonants" in stocks_c tab
+    // TODO: ask about logic F39: area sols arbustifs stocks_c tab.
+    "zones humides": ["411", "412", "421", "422", "423", "511", "512", "521", "522", "523"]
+  }
+  const clcCodes = aldoTypeToClcCodes[groundType]
+  if (!clcCodes) {
+    throw new Error(`No CLC code mapping found for ground type '${groundType}'`)
+  }
+  const csvFilePath = './data/dataByEpci/clc18.csv'
+  const areasByClcType = await csv().fromFile(csvFilePath)
+  const areaForSiren = areasByClcType.find(data => data.siren === location.epci)
+  let totalArea = 0
+  for (const clcCode of clcCodes) {
+    const area = areaForSiren[clcCode]
+    // TODO: output warnings for codes that aren't in data at all? As opposed to empty value
+    if (area) {
+      totalArea += parseFloat(area)
+    }
+  }
+  return totalArea
+}
+
 module.exports = {
-  getGroundCarbonDensity,
+  getCarbonDensity,
+  getArea,
 }
--- data/index.test.js
+++ data/index.test.js
@@ -1,8 +1,24 @@
-const { getGroundCarbonDensity } = require("./index")
+const { getCarbonDensity, getArea } = require("./index")
 
 
 test('returns ground carbon density (as tC/ha) given valid ground type and EPCI SIREN', async () => {
-  expect(await getGroundCarbonDensity({epci: "200000172"}, "cultures")).toBe(54.63)
+  expect(await getCarbonDensity({epci: "200000172"}, "cultures")).toBe(54.63)
+})
+
+test('returns area in hectares (ha) for ground type "cultures" and valid EPCI SIREN', async () => {
+  expect(await getArea({epci: "200000172"}, "cultures")).toBe(1740.7313534999998)
+})
+
+test('returns area in hectares (ha) for ground type "prairies" and valid EPCI SIREN', async () => {
+  expect(await getArea({epci: "200000172"}, "prairies")).toBe(2599.15354877)
+})
+
+test('throws useful error when attempting to get area for ground type without type to CLC type mapping', async () => {
+  try {
+    await getArea({epci: "200000172"}, "lake")
+  } catch (error) {
+    expect(error.message).toBe("No CLC code mapping found for ground type 'lake'")
+  }
 })
 
 // test('returns biomass carbon density (as tC/ha) given valid ground type and EPCI SIREN', () => {
