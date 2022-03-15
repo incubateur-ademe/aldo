f292aa721c713f587682fe31d1a50fcc585f1b23
Helen Root <helenroot1@gmail.com>
2022-03-15 12:38:21 +0100

    Move colours to constants file

--- calculations/constants.js
+++ calculations/constants.js
@@ -36,5 +36,67 @@ module.exports = {
       stocksId: 'haies',
       name: 'Haies'
     }
-  ]
+  ],
+  Colours: {
+    bourgeon: { // green
+      main: '#68A532',
+      '950': '#C9FCAC',
+    },
+    ecume: { // blue
+      main: '#465F9D',
+      '950': '#E9EDFE',
+    },
+    glycine: { // purple
+      main: '#A55A80',
+      '950': '#FEE7FC',
+    },
+    caramel: { // brown
+      main: '#C08C65',
+      '950': '#F7EBE5',
+    },
+    verveine: { // green
+      main: '#B7A73F',
+      '950': '#fceeac',
+    },
+    terre: { // orange
+      main: '#E4794A',
+      '950': '#fee9e5',
+    },
+    emeraude: { // green
+      main: '#00A95F',
+      '950': '#c3fad5',
+    },
+    macaron: { // pink
+      main: '#E18B76',
+      '950': '#fee9e6',
+    },
+    menthe: { // green
+      main: '#009081',
+      '950': '#bafaee',
+    },
+    tournesol: { // yellow
+      main: '#C8AA39',
+      '950': '#feecc2',
+    },
+    archipel: { // green
+      main: '#009099',
+      '950': '#c7f6fc',
+    },
+    tuile: { // pink
+      main: '#CE614A',
+      '950': '#fee9e7',
+    },
+    opera: { // brown
+      main: '#BD987A',
+      '950': '#f7ece4',
+    },
+    cumulus: { // blue
+      main: '#417DC4',
+      '950': '#e6eefe',
+    },
+    moutard: { // yellow
+      main: '#C3992A',
+      '950': '#feebd0',
+    }
+  }
 }
