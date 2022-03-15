32e9219b4e494c1e5e263544e5ee50c5b0637f23
Helen Root <helenroot1@gmail.com>
2022-03-15 12:30:56 +0100

    More colours

--- front/routes.js
+++ front/routes.js
@@ -26,13 +26,35 @@ router.get('/territoire', async (req, res) => {
     '#C9FCAC', // green-bourgeon-950
     '#E9EDFE', // blue-ecume-950
     '#FEE7FC', // purple-glycine-950
-    '#F7EBE5' // brown-caramel-950
+    '#F7EBE5', // brown-caramel-950
+    '#fceeac', // green-tilleul-verveine-950
+    '#fee9e5', // orange-terre-battue-950
+    '#c3fad5', // green-emeraude-950
+    '#fee9e6', // pink-macaron-950
+    '#bafaee', // green-menthe-950
+    '#feecc2', // yellow-tournesol-950
+    '#c7f6fc', // green-archipel-950
+    '#fee9e7', // pink-tuile-950
+    '#f7ece4', // brown-opera-950
+    '#e6eefe', // blue-cumulus-950
+    '#feebd0', // yellow-moutard-950
   ]
   const chartBorderColors = [
     '#68A532', // green-bourgeon-main-640
     '#465F9D', // blue-ecume-main-400
     '#A55A80', // purple-glycine-main-494
-    '#C08C65' // brown-caramel-main-648
+    '#C08C65', // brown-caramel-main-648
+    '#B7A73F', // green-tilleul-verveine-main-707
+    '#E4794A', // orange-terre-battue-main-645
+    '#00A95F', // green-emeraude-main-632
+    '#E18B76', // pink-macaron-main-689
+    '#009081', // green-menthe-main-548
+    '#C8AA39', // yellow-tournesol-main-731
+    '#009099', // green-archipel-main-557
+    '#CE614A', // pink-tuile-main-556
+    '#BD987A', // brown-opera-main-680
+    '#417DC4', // blue-cumulus-main-526
+    '#C3992A', // yellow-moutarde-main-679
   ]
   res.render('territoire', {
     pageTitle: `${epci.nom || 'EPCI pas trouvé'}`,
