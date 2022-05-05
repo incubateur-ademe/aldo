6050914c153735f91d518de72b73fb55ccc40899
Helen Root <helenroot1@gmail.com>
2022-05-05 16:22:38 +0200

    N2O

--- calculations/flux/index.js
+++ calculations/flux/index.js
@@ -16,6 +16,10 @@ function multiplier (from) {
   }
 }
 
+function convertN2O (flux) {
+  return flux < 0 ? -flux / 15 * 0.01 * 44 / 25 - flux / 15 * 0.3 * 0.0075 * 44 / 28 : undefined
+}
+
 function getAnnualFluxes (location, options) {
   const allFluxes = getAllAnnualFluxes(location, options)
   allFluxes.forEach((flux) => {
@@ -28,7 +32,28 @@ function getAnnualFluxes (location, options) {
       const annualtC = flux.flux * area
       flux.value = annualtC
     }
-    flux.co2e = convertToCo2e(flux.value)
+    if (flux.gas === 'C') {
+      flux.co2e = convertToCo2e(flux.value)
+    } else {
+      flux.co2e = flux.value
+    }
+  })
+  // need to do a second pass because N2O calculation requires the sum of ground and litter values
+  const groundFluxes = allFluxes.filter(flux => flux.reservoir === 'ground')
+  groundFluxes.forEach((groundFlux) => {
+    const litterFlux = allFluxes.find(flux => flux.reservoir === 'litter' && flux.from === groundFlux.from && flux.to === groundFlux.to) || {}
+    const groundFluxValue = groundFlux.value || 0
+    const litterFluxValue = litterFlux.value || 0
+    if (groundFluxValue + litterFluxValue < 0) {
+      // decided to keep this grouping because N2O only tracked if emitted
+      allFluxes.push({
+        from: groundFlux.from,
+        to: groundFlux.to,
+        value: convertN2O(groundFluxValue + litterFluxValue),
+        gas: 'N2O'
+        // flux and reservoir don't make much sense here
+      })
+    }
   })
   return allFluxes
 }
--- calculations/flux/integration.test.js
+++ calculations/flux/integration.test.js
@@ -8,26 +8,12 @@ test('returns expected number of entries for cultures ground changes', () => {
   expect(cGround.length).toBe(7)
 })
 
-// test('returns expected number of entries for cultures biomass changes', () => {
-//   const allFlux = getAnnualFluxes({ epci: '200007177' })
-//   const culturesFlux = allFlux.filter(f => f.to === 'cultures')
-//   const cBiomass = culturesFlux.filter(f => f.gas === 'C' && f.reservoir === 'biomass')
-//   expect(cBiomass.length).toBe(7)
-// })
-
 test('returns expected number of entries for cultures litter changes', () => {
   const allFlux = getAnnualFluxes({ epci: '200007177' })
   const culturesFlux = allFlux.filter(f => f.to === 'cultures')
   const litter = culturesFlux.filter(f => f.gas === 'C' && f.reservoir === 'litter')
   expect(litter.length).toBe(2)
 })
-
-// test('returns expected number of entries for cultures n2o changes', () => {
-//   const allFlux = getAnnualFluxes({ epci: '200007177' })
-//   const culturesFlux = allFlux.filter(f => f.to === 'cultures')
-//   const n2o = culturesFlux.filter(f => f.gas === 'N2O')
-//   expect(n2o.length).toBe(8)
-// })
 
 // TODO: make all flux have a value and a co2e value - sums are done on co2e
 
@@ -40,12 +26,14 @@ test('returns expected flux for each prairies -> cultures ground changes', () =>
   expect(cPrairies[0].value + cPrairies[1].value + cPrairies[2].value).toBeCloseTo(-638.71, 2)
 })
 
-// test('returns expected flux for each prairies -> cultures N2O changes', () => {
-//   const allFlux = getAnnualFluxes({ epci: '200007177' })
-//   const culturesFlux = allFlux.filter(f => f.to === 'cultures')
-//   const prairies = culturesFlux.filter(f => f.from.startsWith('prairies'))
-//   const n2oPrairies = prairies.filter(f => f.gas === 'N2O')
-//   expect(n2oPrairies[0].value + n2oPrairies[1].value + n2oPrairies[2].value).toBeCloseTo(0.9, 2)
-// })
+test('returns expected flux for each prairies -> cultures N2O changes', () => {
+  const allFlux = getAnnualFluxes({ epci: '200007177' })
+  const culturesFlux = allFlux.filter(f => f.to === 'cultures')
+  const prairies = culturesFlux.filter(f => f.from.startsWith('prairies'))
+  const n2oPrairies = prairies.filter(f => f.gas === 'N2O')
+  // TODO: the table uses positive values for N2O emission, but negative values for C emission.
+  // chose one direction for simplicity
+  expect(n2oPrairies[0].value + n2oPrairies[1].value).toBeCloseTo(0.9, 2)
+})
 
 // TODO: add a forest litter value test if find EPCI with numbers !== 0
