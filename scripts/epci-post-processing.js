802089b789cd9078cc9a001d8e5ce0c216eccc8a
Helen Root <helenroot1@gmail.com>
2023-02-03 14:54:59 +0100

    Supprimer fichier doublon

--- scripts/epci-post-processing.js
+++ scripts/epci-post-processing.js
@@ -1,7 +1,6 @@
 // This script combines the data in two files containing EPCI data.
 // This is done once in advance rather than per-request since this data does not change per-request.
 // The JSON versions of the files must be generated before this script is run using the csvtojson.sh script.
-// After generation, the france.json file must be moved manually to the data/dataByEpci folder.
 
 const fs = require('fs')
 const epcis = require('../data/dataByEpci/epci.csv.json')
@@ -35,7 +34,7 @@ try {
   const data = JSON.stringify(epciLookup, null, 2)
 
   // write file to disk
-  fs.writeFileSync('france.json', data, 'utf8')
+  fs.writeFileSync('../data/dataByEpci/france.json', data, 'utf8')
 
   console.log('EPCI file successfully generated')
 } catch (err) {
